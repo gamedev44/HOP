2ac87df4e59c334b906449d5b3120ffa782461d6
gamedev44 <108845184+gamedev44@users.noreply.github.com>
2024-09-10 17:49:41 -0500

    added update

--- App/HOP/next.config.mjs
+++ App/HOP/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e3206cb200e264fdddb4dbb2e4772daebc839fdbc94b78316d74892a0de6fa2
-size 92
+oid sha256:d1777dcb3cf831f88117fbe844ecfe320b7daabf577418e1e0cc82048e4f0cfb
+size 91
